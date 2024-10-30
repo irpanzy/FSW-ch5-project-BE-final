c0dff2c53d52e010a1da51aedc3bc4c235107e55
pasipasipada <irfanoputra27@gmail.com>
2024-10-30 08:38:59 +0700

    feat : add decrypt password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
-const { Auth, Users } = require("../models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { Auths, Users } = require("../models");
 
 const register = async (req, res, next) => {
   try {
@@ -11,12 +13,67 @@ const register = async (req, res, next) => {
 
 const login = async (req, res, next) => {
   try {
-    res.status(200).json({
-      status: "Success",
-      message: "Berhasil login",
-      data: token,
+    const { email, password } = req.body;
+
+    const data = await Auths.findOne({
+      include: [
+        {
+          model: Users,
+          as: "user",
+        },
+      ],
+      where: {
+        email,
+      },
     });
-  } catch (err) {}
+
+    if (!data) {
+      return res.status(404).json({
+        status: "Failed",
+        message: "User does not exist",
+        isSuccess: false,
+        data: null,
+      });
+    }
+
+    if (data && bcrypt.compareSync(password, data.password)) {
+      const token = jwt.sign(
+        {
+          id: data.id,
+          username: data.user.name,
+          email: data.email,
+          userId: data.user.id,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: process.env.JWT_EXPIRED,
+        }
+      );
+      return res.status(200).json({
+        status: "Success",
+        message: "Berhasil login",
+        isSuccess: true,
+        data: {
+          username: data.user.name,
+          token,
+        },
+      });
+    } else {
+      res.status(401).json({
+        status: "Failed",
+        message: "Wrong password",
+        isSuccess: false,
+        data: null,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: "Failed",
+      message: err.message,
+      isSuccess: false,
+      data: null,
+    });
+  }
 };
 
 const authenticate = async (req, res) => {
